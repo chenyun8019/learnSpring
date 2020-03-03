31c35980f36a6874a78f03ac919c38c134a86684
chenyun8019 <283419440@qq.com>
2020-03-03 14:30:41 +0800

    修改NewTest.java名为TestSuite.java

---
+++ LearnTestNG/src/main/java/com/testng/series/TestSuite.java
@@ -0,0 +1,22 @@
+package com.testng.series;
+
+import org.testng.annotations.Test;
+import org.testng.annotations.BeforeSuite;
+import org.testng.annotations.AfterSuite;
+
+public class TestSuite {
+  @Test
+  public void test1() {
+	  System.out.println("这是一个测试用例");
+  }
+  @BeforeSuite
+  public void beforeSuite() {
+	  System.out.println("在测试套件前运行");
+  }
+
+  @AfterSuite
+  public void afterSuite() {
+	  System.out.println("在测试套件后运行");
+  }
+
+}
