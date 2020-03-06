a6570735988852ce28d06a444dda018e68171eb5
chenyun8019 <283419440@qq.com>
2020-03-06 22:22:36 +0800

    添加带参数的get请求-两种方法

--- learnSpring/src/main/java/com/jj/controller/springController.java
+++ learnSpring/src/main/java/com/jj/controller/springController.java
@@ -1,5 +1,7 @@
 package com.jj.controller;
 
+import java.util.HashMap;
+import java.util.Map;
 import java.util.Objects;
 
 import javax.security.auth.message.callback.SecretKeyCallback.Request;
@@ -7,12 +9,14 @@ import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-public class springController {
+public class springController<B, A> {
 	@RequestMapping(value="/getwithoutpara",method=RequestMethod.GET)
 	public String toGet(HttpServletResponse re){
 //		HttpServletResponse re	
@@ -36,5 +40,38 @@ public class springController {
 		return "需要携带Cookie访问2";
 		
 	}
+	
+	
+	@RequestMapping(value="/getwithpara1",method=RequestMethod.GET)
+	public Map<String,String> getMap(@RequestParam Integer s,@RequestParam Integer e){
+		
+		Map<String,String> list=new HashMap();
+		list.put("第一", "the first");
+		list.put("第二", "the second");
+		list.put("第三", "the third");
+		
+		return list;
+	}
+	
+	@RequestMapping(value="/getwithpara",method=RequestMethod.GET)
+	public Map<String,String> getMap1(@RequestParam Integer s,@RequestParam Integer e){
+		
+		Map<String,String> list=new HashMap();
+		list.put("age", "12");
+		list.put("ad", "hunan");
+		
+		return list;
+	}
+	
+	
+	@RequestMapping(value="/getwithpara11/{a}/{b}",method=RequestMethod.GET)
+	public Map get(@PathVariable Integer a,@PathVariable Integer b){
+		Map<String,String> li=new HashMap();
+		li.put("one", "hello");
+		li.put("two", "world");
+		
+		return li;		
+	}
+	
 
 }
